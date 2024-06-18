fa1e40863adce154106058d06085a58de63230fc
MbuviDev <mbuvi.meshack99@gmail.com>
2024-06-18 09:44:05 +0300

    first commit

---
+++ weather.js
@@ -0,0 +1,12 @@
+const kelvin = 273
+const celsius = kelvin -273
+console.log(celsius)
+
+let fahrenheit = celsius * (9/5) + 32
+fahrenheit = Math.floor(fahrenheit)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+let newton = celsius * (33/100)
+newton = Math.floor(newton)
+console.log(`The temperature is ${newton} degrees Newton.`)  // Newton scale is not
+
